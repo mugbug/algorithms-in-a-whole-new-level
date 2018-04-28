aadf10ba709399955ac9fa5b4cbdd20e3c8fc227
Pedro Zaroni <zaronipedro@outlook.com>
2018-04-28 15:16:27 -0300

    Add trivial gcd algorithm

--- number_theory/greatest_common_divisor.py
+++ number_theory/greatest_common_divisor.py
@@ -1,8 +1,17 @@
-def gcd(a, b):
+# O(min(a, b))
+def gcd_trivial(a, b):
     minimun = min([a,b])
-    # ...
+    gcd = 0
+    for i in range(1, minimun+1):
+        if (a % i) == 0 and (b % i) == 0:
+            gcd = i
+    return gcd
 
+
+# O(log(min(a, b)))
 def euclid(a, b):
     return a if b == 0 else euclid(b, a % b)
 
-print(euclid(13, 23))
+if __name__ == '__main__':
+    print('Trivial:', gcd_trivial(30, 15))
+    print('Euclid:', euclid(41232, 512))
