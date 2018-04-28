61eb0231e6495617f83b86db2b7b583fd73f7c6c
Pedro Zaroni <zaronipedro@outlook.com>
2018-04-28 19:58:15 -0300

    Add Fenwick tree implementation

--- advanced_data_structures/binary_indexed_tree.py
+++ advanced_data_structures/binary_indexed_tree.py
@@ -1,24 +1,38 @@
+# tip: i & (-i) returns the least bit 1
+
+# stackoverflow explanation, for i = 44
+# i           = 0000 0000 0000 0000 0000 0000 0010 1100
+# ~i          = 1111 1111 1111 1111 1111 1111 1101 0011
+# -i = (~i)+1 = 1111 1111 1111 1111 1111 1111 1101 0100
+# (i) & (-i)  = 0000 0000 0000 0000 0000 0000 0000 0100
+
 def construct_bit(arr, n):
     # constructs and returns a BIT for a
     # given array 'arr' of size 'n'
-
-    # - step 1:
-    #   root index = 0 (always with value 0 as well)
-    # - step 2:
-    #   subsequent nodes' parents will be
-    #       current node binary index with rightest 1 cleared
     for index in range(n):
-        pass
+        update(index, arr[index])
 
 def get_sum(index):
     # returns the sum of 'arr' elements
-    pass
+    ans = 0
+    index += 1
+    while index > 0:
+        ans += bit[index]
+        index -= index & (-index)
+    return ans
 
 def update(index, val):
     # updates BIT at given index with given value 'val'
-    i = 0  
-    pass
+    index += 1
+    while index <= len(arr):
+        bit[index] += val
+        index += index & (-index)
+        # same as
+        # index += index & (~index + 1)
 
-arr = [3, 2, -1, 6, 5, 4, -3]
-bit = [0]*(len(arr)+1)
-construct_bit(arr, len(arr))
+if __name__ == '__main__':
+    arr = [3, 2, -1, 6, 5, 4, -3]
+    bit = [0]*(len(arr)+1)
+    construct_bit(arr, len(arr))
+    update(2, 6)
+    print(bit)
